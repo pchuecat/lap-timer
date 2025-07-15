f3fcaec222177f0b029324736011001f38879c10
pchuecat <pablo.chueca.taus@gmail.com>
2025-07-15 22:17:36 +0200

    feat: added formatter

--- telemetry/src/routes/+page.server.ts
+++ telemetry/src/routes/+page.server.ts
@@ -1,49 +1,45 @@
 export const ssr = false
 
-import { parseNmeaSentence } from 'nmea-simple';
+import { parseNmeaSentence } from 'nmea-simple'
 
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async () => {
-  const file = Bun.file('../nmea/circuito-internacional-zuera-10hz-2m.nmea')
-  const lines = (await file.text()).split(/\r?\n/)
+	const file = Bun.file('../nmea/circuito-internacional-zuera-10hz-2m.nmea')
+	const lines = (await file.text()).split(/\r?\n/)
 
-  const coords: [number, number][] = []
+	const coords: [number, number][] = []
 
-  for (const line of lines) {
-    try {
-      const packet = parseNmeaSentence(line)
-      if (packet.sentenceId === 'RMC' && packet.status === 'valid') {
-        coords.push([packet.latitude, packet.longitude])
-      }
-    } catch { }
-  }
+	for (const line of lines) {
+		try {
+			const packet = parseNmeaSentence(line)
+			if (packet.sentenceId === 'RMC' && packet.status === 'valid') {
+				coords.push([packet.latitude, packet.longitude])
+			}
+		} catch {}
+	}
 
-  return { coords: smoothCoords(coords) }
+	return { coords: smoothCoords(coords) }
 }
 
 function smoothCoords(coords: [number, number][], windowSize = 5): [number, number][] {
-  const smoothed: [number, number][] = [];
+	const smoothed: [number, number][] = []
 
-  for (let i = 0; i < coords.length; i++) {
-    let latSum = 0;
-    let lngSum = 0;
-    let count = 0;
+	for (let i = 0; i < coords.length; i++) {
+		let latSum = 0
+		let lngSum = 0
+		let count = 0
 
-    for (
-      let j = i - Math.floor(windowSize / 2);
-      j <= i + Math.floor(windowSize / 2);
-      j++
-    ) {
-      if (j >= 0 && j < coords.length) {
-        latSum += coords[j][0];
-        lngSum += coords[j][1];
-        count++;
-      }
-    }
+		for (let j = i - Math.floor(windowSize / 2); j <= i + Math.floor(windowSize / 2); j++) {
+			if (j >= 0 && j < coords.length) {
+				latSum += coords[j][0]
+				lngSum += coords[j][1]
+				count++
+			}
+		}
 
-    smoothed.push([latSum / count, lngSum / count]);
-  }
+		smoothed.push([latSum / count, lngSum / count])
+	}
 
-  return smoothed;
+	return smoothed
 }
--- telemetry/src/routes/+page.svelte
+++ telemetry/src/routes/+page.svelte
@@ -1,31 +1,21 @@
 <script lang="ts">
-  import L from "leaflet";
+	import L from 'leaflet'
 
-  import { onMount } from "svelte";
+	import { onMount } from 'svelte'
 
-  let { data } = $props();
+	let { data } = $props()
 
-  onMount(async () => {
-    const map = L.map("map", { attributionControl: false });
+	onMount(async () => {
+		const map = L.map('map', { attributionControl: false })
 
-    var polyline = L.polyline(data.coords, {
-      color: "blue",
-      weight: 10,
-    }).addTo(map);
+		var polyline = L.polyline(data.coords, { color: 'blue', weight: 10 }).addTo(map)
 
-    L.tileLayer("https://tile.osm.org/{z}/{x}/{y}.png", {
-      maxZoom: 20,
-      maxNativeZoom: 19,
-    }).addTo(map);
+		L.tileLayer('https://tile.osm.org/{z}/{x}/{y}.png', { maxZoom: 20, maxNativeZoom: 19 }).addTo(map)
 
-    map.fitBounds(polyline.getBounds());
-  });
+		map.fitBounds(polyline.getBounds())
+	})
 </script>
 
-<div
-  class="absolute bottom-4 left-4 w-48 z-[999] rounded-lg p-2 backdrop-blur-xs bg-zinc-600/70 text-white"
->
-  test
-</div>
+<div class="absolute bottom-4 left-4 z-[999] w-48 rounded-lg bg-zinc-600/70 p-2 text-white backdrop-blur-xs">test</div>
 
 <div id="map" class="h-full w-full"></div>
